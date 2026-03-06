7fb347526e60831e7d7dcf8967e55586d281c48d
Wondermutt-v1 <71840008+Wondermutt-v1@users.noreply.github.com>
2026-03-06 10:19:00 -0600

    Add HelloWorld.py with a greeting example

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+#
+# Example file for Helloworld
+# LinkedIn Learning Python course
+#
+
+def main():
+    print("Hello World")
+    name = input("What is your name?")
+    print("Nice to meet you", name)
+
+if __name__ == "__main__":
+    main()
