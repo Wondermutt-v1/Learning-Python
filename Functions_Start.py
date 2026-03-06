a97f366a75c97311ab607a28aed4504a74187a1f
Wondermutt-v1 <71840008+Wondermutt-v1@users.noreply.github.com>
2026-03-06 10:16:16 -0600

    Define basic function and add example calls
    Added a basic function definition and example calls.

---
+++ Functions_Start.py
@@ -0,0 +1,24 @@
+#
+# Example file for working with functions
+# LinkedIn Learning Python course
+#
+
+# TODO : DEFINE A BASIC FUNCTION
+def func1():
+    print("I am a function")
+
+# TODO: function that takes arguments
+
+
+#TODO function that returns a value
+
+
+# TODO function with default value for an argument
+
+
+# TODO: function with variable number of arguments
+
+
+func1()
+print(func1())
+print(func1)
