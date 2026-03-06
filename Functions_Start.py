8bcdcd08e81de7a14a4ccab9c4b2b18336af61bb
Wondermutt-v1 <adrian.r.perez@outlook.com>
2026-03-06 10:43:17 -0600

    Update Functions_Start.py

--- Functions_Start.py
+++ Functions_Start.py
@@ -8,13 +8,19 @@ def func1():
     print("I am a function")
 
 # TODO: function that takes arguments
-
+def func2(arg1, arg2):
+    print(arg1, " ", arg2)
 
 #TODO function that returns a value
-
+def cube(x):
+    return x*x*x
 
 # TODO function with default value for an argument
-
+def power(num, x=1):
+    result = 1
+    for i in range(x):
+        result = result * num
+    return result
 
 # TODO: function with variable number of arguments
 
@@ -22,3 +28,7 @@ def func1():
 func1()
 print(func1())
 print(func1)
+
+func2(10,20)
+print(func2(10,20))
+print(cube(3))
