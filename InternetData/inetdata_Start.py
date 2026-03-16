806c28158a7c5a28e0a6c3fd9b81a245584b2d79
Wondermutt-v1 <adrian.r.perez@outlook.com>
2026-03-16 15:24:49 -0500

    Update inetdata_Start.py

---
+++ InternetData/inetdata_Start.py
@@ -0,0 +1,18 @@
+#
+# Example file for retrieving data from the Internet
+# LinkedIn Learning Python course
+#
+
+import urllib.request
+
+def main():
+    #pass # this is a placeholder, do nothing statement
+
+    weburl = urllib.request.urlopen("http://www.google.com")
+    print("result code:", weburl.getcode())
+    data = weburl.read()
+    print(data)
+    
+
+if __name__ == "__main__":
+    main()
