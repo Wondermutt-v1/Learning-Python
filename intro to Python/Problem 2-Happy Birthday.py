725debd356f184107674c066de8edbf87044071c
Wondermutt-v1 <adrian.r.perez@outlook.com>
2026-03-20 11:32:22 -0500

    Create Problem 2-Happy Birthday.py

---
+++ intro to Python/Problem 2-Happy Birthday.py
@@ -0,0 +1,27 @@
+#
+# Problem 2: Happy Birthday
+# Introduction to Python from Linkedin Learning
+#
+
+# TODO: Write a program that asks the user for their name and then prints out "Happy Birthday, [name]!" where [name] is the name they entered.When you run the program, it should look like this:
+birthday_person = input("Name: ").strip().title()   ## strip() removes any leading or trailing whitespace from the input
+
+print("Happy Birthday, " + birthday_person + "!")
+
+print("Happy Birthday to you!")
+print("Happy Birthday to you!")
+print("Happy Birthday dear " + birthday_person + "!")
+print("Happy Birthday to you!")
+
+
+# # TODO: look at the uses of string functions
+# string = "Hello World"
+# print(string)
+# string = string.upper()
+# print(string)
+# string = string.lower()
+# print(string)
+# string = string.title() # string = string.capitalize() # only the first letter of the first word is capitalized
+# print(string)
+# string = string.replace(" ", "_")
+# print(string)
