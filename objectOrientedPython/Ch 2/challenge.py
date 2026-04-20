abbfc90dcdd70e0c769a979fa675c34f1e544a5d
Wondermutt-v1 <adrian.r.perez@outlook.com>
2026-04-20 16:24:14 -0500

    Update challenge.py

--- objectOrientedPython/Ch 2/challenge.py
+++ objectOrientedPython/Ch 2/challenge.py
@@ -12,14 +12,37 @@
 # For stocks: "Ticker: Company -- $Price"
 # For bonds: "description: duration'yr' : $price : yieldamt%"
 
-class Asset():
-    pass
+from abc import ABC, abstractmethod
 
-class Stock():
-    pass
+class Asset(ABC):
+    def __init__(self, price):
+        self.price = price
+        
+        @abstractmethod
+        def get_description(self):
+            pass
+        
 
-class Bond():
-    pass
+
+class Stock(Asset):
+    def __init__(self, ticker, price, company):
+        super().__init__(price)
+        self.company = company
+        self.ticker = ticker
+        
+    def get_description(self):
+        return f"{self.ticker}: {self.company} -- ${self.price}"
+
+
+class Bond(Asset):
+    def __init__(self, price, description, duration, interest):
+        super().__init__(price)
+        self.description = description
+        self.duration = duration
+        self.interest = interest
+        
+    def get_description(self):
+        return f"{self.description} : {self.duration} :  ${self.price}"
 
 
 # ~~~~~~~~~ TEST CODE ~~~~~~~~~
